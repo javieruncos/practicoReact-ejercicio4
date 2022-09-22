2b51b111b3a237e32ca0f05b8bd37f17d2b9acc1
Javier Uncos <javiersrfc30@gmail.com>
2022-09-22 20:25:35 -0300

    agregamos el componente del fomulario

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Formulario = (props) => {
+    return (
+        <div>
+            <h2>{props.titulo}</h2>
+        </div>
+    );
+};
+
+export default Formulario;
