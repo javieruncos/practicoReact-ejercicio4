f0562d56972514916456a0f830d52f8c14ef7dbd
Javier Uncos <javiersrfc30@gmail.com>
2022-09-22 23:07:28 -0300

    agregamos las tareas en el arreglo de tareas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Formulario from './components/Formulario';
-import ListaTarea from './components/ListaTarea';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container } from 'react-bootstrap';
 
@@ -13,7 +12,6 @@ function App() {
       <div>
        <h1 className='display-4'>Lista de tareas</h1>
        <Formulario titulo="hola mundo"/>
-       <ListaTarea/>
       </div>
       </Container>
     </div>
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import { useState } from 'react';
 import { Form,Button } from 'react-bootstrap';
+import ListaTarea from './ListaTarea';
 
 const Formulario = () => {
+
+
+     const handleSubmit = (e)=>{
+       e.preventDefault();
+       setarregloTarea([...arregloTarea,tarea]);
+       setTarea("")
+     }
+
+
+
+
+    const [tarea, setTarea] = useState("")
+    const [arregloTarea, setarregloTarea] = useState([])
     return (
-       <Form>
+        <>
+       <Form onSubmit={handleSubmit}>
            <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-           <Form.Control type="email" placeholder="Enter email" />
-           <Button variant="primary" type='submit'>
-            enviar
-           </Button>
-      </Form.Group>
-       </Form>
+               <Form.Control type="text"
+                placeholder="Ingresa una tarea" 
+                onChange={(e)=>setTarea(e.target.value)}
+                value={tarea} />
+               <Button variant="primary" type='submit'>
+              enviar
+             </Button>
+           </Form.Group>
+       </Form> 
+        <ListaTarea/>
+        </>
     );
 };
 
